b19786d60b7cf96a85d3044b48df4705bc54458c
sobe52 <sobemomin2@gmai.com>
2024-01-04 01:12:06 -0800

    127:creat save post Feature

---
+++ mybloog11/src/main/java/com/mybloog/mybloog11/Mybloog11Application.java
@@ -0,0 +1,13 @@
+package com.mybloog.mybloog11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mybloog11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mybloog11Application.class, args);
+	}
+
+}
---
+++ mybloog11/src/test/java/com/mybloog/mybloog11/Mybloog11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mybloog.mybloog11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mybloog11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
